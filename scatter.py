d0bc6a68c2062b89f4761c53ec2cd5a670e38ef6
Jon Proctor <jon.proctor@phototopo.com>
2023-01-06 13:19:19 -0700

    Update scatter.py
    removed an un needed loop

--- scatter.py
+++ scatter.py
@@ -1,14 +1,9 @@
 import matplotlib.pyplot as plt
 
-numSamp = 21000
-
-for i in range(numSamp):
-    r = random.rand()
-    print(r)
-print("\n") # blank line
-
+numSamp = 2100
 mu_x, sigma_x = 0, 6 # mean and standard deviation
-mu_y, sigma_y = -2, 4
+mu_y, sigma_y = -2, 22
+
 x = np.random.normal(mu_x, sigma_x, numSamp)
 y = np.random.normal(mu_y, sigma_y, numSamp)
 print(x)
