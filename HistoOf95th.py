8bc6691fb8bda20a4333a8a9cd823b40aecec1c1
Jon Proctor <jon.proctor@phototopo.com>
2023-10-27 16:22:48 -0600

    Update HistoOf95th.py
    updates for limits and abs value of x

--- HistoOf95th.py
+++ HistoOf95th.py
@@ -1,28 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-numTrys = 10000
-measureX = ([])
-measureX.clear()
+itter8 = 100000
+stdx = ([])
+stdx.clear()
 nfx = ([])
 nfx.clear()
 
-numSamp = 20
+numSamp = 32
 
 mu_x, sigma_x = 0, 1 # mean and standard deviation
 
 # For Histogram graph
-numBins = 31
+numBins = 51
 histDensity = 'TRUE'
 histOrientation = 'vertical'  # 'vertical' or 'horizontal'
+xMin = 0  # adding limits to keep histogram range consistent
+xMax = 4
 
-for i in range(numTrys):
+for i in range(itter8):
     x = np.random.normal(mu_x, sigma_x, numSamp)
     # print(np.std(x))
-    measureX.insert(i,np.std(x))
-    nfx.insert(i,np.percentile(x,95))
+    stdx.insert(i,np.std(x))
+    nfx.insert(i,np.percentile(abs(x), 95))  # absolute value of x
 
 # plotting
-plt.title("histogram")
+plt.title(numSamp)
 plt.hist(nfx, bins = numBins, density = histDensity, orientation = histOrientation)
-plt.show()
+plt.xlim(xMin, xMax) 
+plt.show()# Write your code here :-)
