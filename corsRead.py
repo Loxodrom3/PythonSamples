4ef59eb55dc1f2c88d0d38374144068f600bdcab
Jon Proctor <jon.proctor@phototopo.com>
2024-04-23 10:31:53 -0600

    Update corsRead.py
    Jon and Jonathon colaboration

--- corsRead.py
+++ corsRead.py
@@ -1,16 +1,26 @@
 from urllib.request import urlopen
 
-# replace stationList with a read of inputList.txt
-# get output file
-# open file
-stationList = ["neal", "negi", "neho", "neor", "nerc", "neyk"]
+# Read station list from inputList.txt
+with open("inputList.txt", "r") as file:
+    stationList = file.read().splitlines()
 
+# Get output file
+output_file_path = "CORSoutput.txt"
+output_file = open(output_file_path, "w")
 
-for i in range(0, len(stationList)): #
-    print(stationList[i])
-    url = 'https://www.ngs.noaa.gov/cgi-cors/CorsSidebarSelect.prl?site=' + stationList[i] + '&option=Coordinates14'
+for station in stationList:
+    print(station)
+    url = 'https://www.ngs.noaa.gov/cgi-cors/CorsSidebarSelect.prl?site=' + station + '&option=Coordinates14'
     dataSheet = urlopen(url).read()
-    # error check for URL return
-    print(dataSheet.decode('utf-8'))
-    # write to outputfile
-    # Parse data to what we need
+    # Error check for URL return
+    if dataSheet:   # need a different check for "bad station"
+        data = dataSheet.decode('utf-8')
+        output_file.write(data)
+        output_file.write('\n')  
+        output_file.write('\n')  
+        output_file.write('\n')  
+        # Parse data to what we need
+    else:
+        print("Failed to fetch data for station:", station)
+
+output_file.close()
